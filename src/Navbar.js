bc8a836d7268254e6566b73188112f2b8adce1ff
TouiaBakassi <Touria_Bakassi@outlook.com>
2022-09-27 03:43:33 +0100

    Navbar-ResponsiveD

---
+++ src/Navbar.js
@@ -0,0 +1,35 @@
+import { Component } from "react";
+import './Navbar.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+export class Navbar extends Component{
+  
+  constructor(props){
+super(props);
+this.state={
+display:"none",
+}
+  }
+
+  render(){
+    // let display="none";
+    return( 
+      <nav className="nav">
+<a className="logo">NAVBAR</a>
+<ul className={display}>
+  <li><a href="#">Home</a></li>
+  <li><a href="#">Features</a></li>
+  <li><a href="#">Blog</a></li>
+  <li><a href="#">Contact</a></li>
+</ul>
+<FontAwesomeIcon className="icon" icon={faBars} onClick={()=>{
+  {display=""}
+}} />
+
+      </nav>
+      
+    )
+  }
+}
+
